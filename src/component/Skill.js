bcde387399d9880d756595d971c071baac644271
Oladunni Waters <waters.oladunni@gmail.com>
2023-05-01 17:45:47 +0100

    skills section

--- src/component/Skill.js
+++ src/component/Skill.js
@@ -1,36 +1,34 @@
 import "./Skill.scss";
-
+//import { FaBeer } from 'react-icons/fa';
+//RxCube BiCubeAlt
 
 function Skills() {
   return (
-    <>
     <div className="skills">
        <h1 className='skill-h1'>SKILLS</h1>    
        <div className='skill-cont'>
-          <ul>
+          <div className='list-cont'>
              <li className='skill'>HTML</li>
              <li className='skill'>CSS</li>
              <li className='skill'>Javascript</li>
              <li className='skill'>React.js</li>
-           </ul>
+           </div>
            
-          <ul>
+          <div className='list-cont'>
              <li className='skill'>SASS/SCSS</li>
              <li className='skill'>Tailwind CSS</li>
              <li className='skill' >Redux Toolkit</li>
              <li className='skill'>React Native</li>
-           </ul>
+           </div>
            
-            <ul>
+            <div className='list-cont'>
              <li className='skill'>Next.js</li>
              <li className='skill' >Typescript</li>
              <li className='skill'>MUI</li>
              <li className='skill'>GIT</li>
-           </ul>
+           </div>
        </div>
     </div>   
-   </>
   );
 }
 export default Skills;
-
