26b8f33e227c4a36d62fe682646a78b6f71035eb
Oladunni Waters <waters.oladunni@gmail.com>
2023-05-02 16:46:46 +0100

    feat: added projects screenshot

--- src/component/Work.js
+++ src/component/Work.js
@@ -1,6 +1,11 @@
 import './Work.scss';
 import { FaGreaterThan, FaLessThan  } from 'react-icons/fa';
-import img1 from '../assets/eb8ae42768ae53944c88a3823070a739.webp'
+import img1 from '../assets/Waters-store.png'
+import img2 from '../assets/React-App (1).png'
+import img3 from '../assets/React-App.png'
+import img4 from '../assets/Chart-Dashboard.png'
+
+
 
 function Work() {
 
@@ -20,8 +25,7 @@ function Work() {
                        </p>
                      </div>  
                      <ul className='project-link-cont'>
-                        <li className='project-list'><a href="https://github.com/OladunniWaters" className='project-link'><FaLessThan />Github<FaGreaterThan /></a></li>
-                        <li className='project-list'><a href="https://waters-movie.netlify.app/" className='project-link'><FaLessThan />Website<FaGreaterThan /></a></li>
+                        <li className='project-list'><a href="https://waters-ecommerce.netlify.app/" target="_blank" rel="noreferrer" className='project-link'><FaLessThan />Website<FaGreaterThan /></a></li>
                      </ul>
                   </div>
                </div>
@@ -30,18 +34,17 @@ function Work() {
             
             <div className='project-one'>
                <div>
-                   <img src={img1} alt='...' className='project-img'/>
+                   <img src={img2} alt='...' className='project-img'/>
                </div>
                <div className='project-overlay'>
                   <div className='project-body'>
                     <div className='project-text-cont'>
-                       <h4 className='project-header'>Waters Store</h4>
+                       <h4 className='project-header'>Movie Trailer</h4>
                        <p className='project-desc'>Navigation tool that significantly increases efficiency by reducing the number of clicks it takes you to navaigate he web
                        </p>
                      </div>  
                       <ul className='project-link-cont'>
-                        <li className='project-list'><a href="https://github.com/OladunniWaters" className='project-link'><FaLessThan />Github<FaGreaterThan /></a></li>
-                        <li className='project-list'><a href="https://waters-movie.netlify.app/" className='project-link'><FaLessThan />Website<FaGreaterThan /></a></li>
+                        <li className='project-list'><a href="https://waters-movie.netlify.app/" target="_blank" rel="noreferrer" className='project-link'><FaLessThan />Website<FaGreaterThan /></a></li>
                      </ul>
                   </div>
                </div>
@@ -50,18 +53,17 @@ function Work() {
             
              <div className='project-one'>
                <div>
-                   <img src={img1} alt='...' className='project-img'/>
+                   <img src={img3} alt='...' className='project-img'/>
                </div>
                <div className='project-overlay'>
                   <div className='project-body'>
                     <div className='project-text-cont'>
-                       <h4 className='project-header'>Waters Store</h4>
+                       <h4 className='project-header'>Recipe WebApp</h4>
                        <p className='project-desc'>Navigation tool that significantly increases efficiency by reducing the number of clicks it takes you to navaigate he web
                        </p>
                      </div>  
                    <ul className='project-link-cont'>
-                        <li className='project-list'><a href="https://github.com/OladunniWaters" className='project-link'><FaLessThan />Github<FaGreaterThan /></a></li>
-                        <li className='project-list'><a href="https://waters-movie.netlify.app/" className='project-link'><FaLessThan />Website<FaGreaterThan /></a></li>
+                        <li className='project-list'><a href="https://unrivaled-mochi-952733.netlify.app/" target="_blank" rel="noreferrer" className='project-link'><FaLessThan />Website<FaGreaterThan /></a></li>
                      </ul>
                   </div>
                </div>
@@ -70,18 +72,17 @@ function Work() {
             
              <div className='project-one'>
                <div>
-                   <img src={img1} alt='...' className='project-img'/>
+                   <img src={img4} alt='...' className='project-img'/>
                </div>
                <div className='project-overlay'>
                   <div className='project-body'>
                     <div className='project-text-cont'>
-                       <h4 className='project-header'>Waters Store</h4>
+                       <h4 className='project-header'>Crypto Dashboard</h4>
                        <p className='project-desc'>Navigation tool that significantly increases efficiency by reducing the number of clicks it takes you to navaigate he web
                        </p>
                      </div>  
                    <ul className='project-link-cont'>
-                        <li className='project-list'><a href="https://github.com/OladunniWaters" className='project-link'><FaLessThan />Github<FaGreaterThan /></a></li>
-                        <li className='project-list'><a href="https://waters-movie.netlify.app/" className='project-link'><FaLessThan />Website<FaGreaterThan /></a></li>
+                        <li className='project-list'><a href="https://waters-chart-dashboard.netlify.app/Dashboard"  target="_blank" rel="noreferrer" className='project-link'><FaLessThan />Website<FaGreaterThan /></a></li>
                      </ul>
                   </div>
                </div>
@@ -100,8 +101,7 @@ function Work() {
                        </p>
                      </div>  
                     <ul className='project-link-cont'>
-                        <li className='project-list'><a href="https://github.com/OladunniWaters" className='project-link'><FaLessThan />Github<FaGreaterThan /></a></li>
-                        <li className='project-list'><a href="https://waters-movie.netlify.app/" className='project-link'><FaLessThan />Website<FaGreaterThan /></a></li>
+                        <li className='project-list'><a href="https://waters-movie.netlify.app/" target="_blank" rel="noreferrer" className='project-link'><FaLessThan />Website<FaGreaterThan /></a></li>
                      </ul>
                   </div>
                </div>
@@ -119,8 +119,7 @@ function Work() {
                        </p>
                      </div>  
                     <ul className='project-link-cont'>
-                        <li className='project-list'><a href="https://github.com/OladunniWaters" className='project-link'><FaLessThan />Github<FaGreaterThan /></a></li>
-                        <li className='project-list'><a href="https://waters-movie.netlify.app/" className='project-link'><FaLessThan />Website<FaGreaterThan /></a></li>
+                        <li className='project-list'><a href="https://waters-movie.netlify.app/" target="_blank" rel="noreferrer" className='project-link'><FaLessThan />Website<FaGreaterThan /></a></li>
                      </ul>
                   </div>
                </div>
